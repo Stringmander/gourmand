a76294ceead99820d8bf0671a95b6246731972e4
nrusso21 <nrusso@protonmail.com>
2020-02-05 13:34:07 -0500

    Add favorites & shopping list buttons & additional AppBar styling

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -4,10 +4,12 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import InputBase from '@material-ui/core/InputBase';
-// import Button from '@material-ui/core/Button';
-// import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
 // import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'
 
 import logo from '../assets/img/logo.svg';
 
@@ -15,14 +17,15 @@ const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
   },
-  menuButton: {
-    marginRight: theme.spacing(2),
+  toolbar: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 1fr)'
   },
   title: {
     flexGrow: 1,
     display: 'none',
     [theme.breakpoints.up('sm')]: {
-      display: 'block',
+      display: 'inline',
     },
     fontFamily: 'Pacifico',
   },
@@ -33,13 +36,12 @@ const useStyles = makeStyles(theme => ({
     '&:hover': {
       backgroundColor: fade(theme.palette.common.white, 0.25),
     },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
     width: '100%',
     [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(3),
       width: 'auto',
     },
+    gridColumn: 2,
+    justifySelf: 'center',
   },
   searchIcon: {
     width: theme.spacing(7),
@@ -64,11 +66,22 @@ const useStyles = makeStyles(theme => ({
       },
     },
   },
+  logoWrapper: {
+    display: 'flex',
+  },
   logo: {
-    maxHeight: theme.spacing(5),
-    margin: theme.spacing(0, 2, 0, 2),
+    maxHeight: theme.spacing(4),
+    margin: theme.spacing(0, 2),
+  },
+  buttonsWrapper: {
+    justifySelf: 'flex-end',
+    margin: theme.spacing(0, 2),
+  },
+  button: {
+    color: theme.palette.common.white,
   },
 }));
+
 
 export default function PrimaryAppBar(props) {
   const classes = useStyles();
@@ -76,11 +89,13 @@ export default function PrimaryAppBar(props) {
   return (
     <div className={classes.root}>
       <AppBar position="static">
-        <Toolbar>
-          <img src={logo} className={classes.logo} />
-          <Typography variant="h6" className={classes.title}>
-            Gourmand
-          </Typography>
+        <Toolbar className={classes.toolbar} >
+          <span className={classes.logoWrapper} >
+            <img src={logo} className={classes.logo} />
+            <Typography variant="h5" className={classes.title}>
+              Gourmand
+            </Typography>
+          </span>
           <div className={classes.search}>
             <div className={classes.searchIcon}>
               <SearchIcon />
@@ -95,6 +110,14 @@ export default function PrimaryAppBar(props) {
               onKeyDown={props.handleSearchSubmit}
             />
           </div>
+          <span className={classes.buttonsWrapper} >
+            <IconButton className={classes.button} >
+              <FavoriteIcon />
+            </IconButton>
+            <IconButton className={classes.button} >
+              <ShoppingCartIcon />
+            </IconButton>
+          </span>
         </Toolbar>
       </AppBar>
     </div>
