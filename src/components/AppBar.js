0ddfaf138d37ce7619639bde0b3246fcf3118ecf
nrusso21 <nrusso@protonmail.com>
2020-02-06 17:39:50 -0500

    Add SearchField Component & AppBar media queries

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -1,77 +1,45 @@
 import React from 'react';
-import { fade, makeStyles } from '@material-ui/core/styles';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-// import MenuIcon from '@material-ui/icons/Menu';
+import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'
 
+import SearchField from './SearchField';
+
 import logo from '../assets/img/logo.svg';
+import { useMediaQuery } from '@material-ui/core';
+
 
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
   },
   toolbar: {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(3, 1fr)'
+    display: 'flex',
+    [theme.breakpoints.up('sm')]: {
+      display: 'grid',
+      gridTemplateColumns: 'repeat(3, 1fr)'
+    }
   },
   title: {
     flexGrow: 1,
-    display: 'none',
-    [theme.breakpoints.up('sm')]: {
-      display: 'inline',
-    },
+    display: 'inline',
     fontFamily: 'Pacifico',
   },
-  search: {
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
-    },
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: 'auto',
-    },
-    gridColumn: 2,
-    justifySelf: 'center',
-  },
-  searchIcon: {
-    width: theme.spacing(7),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputRoot: {
-    color: 'inherit',
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 7),
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: 120,
-      '&:focus': {
-        width: 200,
-      },
-    },
+  menuButton: {
+    marginRight: theme.spacing(2),
   },
   logoWrapper: {
     display: 'flex',
   },
   logo: {
     maxHeight: theme.spacing(4),
-    margin: theme.spacing(0, 2),
+    margin: theme.spacing(0, 1),
   },
   buttonsWrapper: {
     justifySelf: 'flex-end',
@@ -83,43 +51,40 @@ const useStyles = makeStyles(theme => ({
 }));
 
 
-export default function PrimaryAppBar(props) {
+function PrimaryAppBar(props) {
+  const theme = useTheme();
   const classes = useStyles();
+
+  const matchesUpSm = useMediaQuery(theme.breakpoints.up('sm'));
+  const matchesDownXs = useMediaQuery(theme.breakpoints.down('xs'))
+
 
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar className={classes.toolbar} >
-          <span className={classes.logoWrapper} >
+          {matchesDownXs && <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>}
+          {matchesUpSm && <span className={classes.logoWrapper} >
             <img src={logo} className={classes.logo} />
-            <Typography variant="h5" className={classes.title}>
+            <Typography variant="h6" className={classes.title}>
               Gourmand
             </Typography>
-          </span>
-          <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              <SearchIcon />
-            </div>
-            <InputBase
-              placeholder="Search…"
-              classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput,
-              }}
-              inputProps={{ 'aria-label': 'search' }}
-              onKeyDown={props.handleSearchSubmit}
-            />
-          </div>
-          <span className={classes.buttonsWrapper} >
+          </span>}
+          <SearchField onKeyDownHandler={props.handleSearchSubmit} />
+          {matchesUpSm && <span className={classes.buttonsWrapper} >
             <IconButton className={classes.button} >
               <FavoriteIcon />
             </IconButton>
             <IconButton className={classes.button} >
               <ShoppingCartIcon />
             </IconButton>
-          </span>
+          </span>}
         </Toolbar>
       </AppBar>
     </div>
   );
 }
+
+export default PrimaryAppBar;
---
+++ src/components/SearchField.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
+
+
+const useStyles = makeStyles(theme => ({
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            width: 'auto',
+        },
+        gridColumn: 2,
+        justifySelf: 'center',
+    },
+    searchIcon: {
+        width: theme.spacing(7),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputRoot: {
+        color: 'inherit',
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 7),
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            width: 120,
+            '&:focus': {
+                width: 200,
+            },
+        },
+    },
+}));
+
+function SearchField(props) {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.search}>
+            <div className={classes.searchIcon}>
+                <SearchIcon />
+            </div>
+            <InputBase
+                placeholder="Search…"
+                classes={{
+                    root: classes.inputRoot,
+                    input: classes.inputInput,
+                }}
+                inputProps={{ 'aria-label': 'search' }}
+                onKeyDown={props.onKeyDownHandler}
+            />
+        </div>
+    );
+}
+
+export default SearchField
